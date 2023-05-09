859d88401da206cdfbcd1c5eb07119712dacd528
hexiao <2608019886@qq.com>
2023-05-09 17:39:10 +0800

    update

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # 检索关键词
-KEY_WORD = '周也'
+KEY_WORD = '张婧仪'
 # 文件存储路径
 SAVE_PATH = f'./result/{KEY_WORD}'
 # 最大并发请求数
-SEMAPHORE = 50
+SEMAPHORE = 60
 
 BASE_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'
